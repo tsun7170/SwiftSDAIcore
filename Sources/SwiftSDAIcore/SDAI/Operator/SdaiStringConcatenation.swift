31eed02ef59ae4e9151f4ca1ba3f1aff5ca2e564
Tsutomu Yoshida <tsuntsunkobi@icloud.com>
2021-03-20 18:11:09 +0900

    implemented string concatenation operator.,

--- Sources/SwiftSDAIcore/SDAI/Operator/SdaiStringConcatenation.swift
+++ Sources/SwiftSDAIcore/SDAI/Operator/SdaiStringConcatenation.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
-//MARK: - String concatenation operator
-public func + <T: SDAIStringType, U: SDAIStringType>(lhs: T?, rhs: U?) -> SDAI.STRING? { abstruct() }
-public func + <T: SDAIStringType, U: SDAISelectType>(lhs: T?, rhs: U?) -> SDAI.STRING? { abstruct() }
-public func + <T: SDAISelectType, U: SDAIStringType>(lhs: T?, rhs: U?) -> SDAI.STRING? { abstruct() }
-public func + <T: SDAIStringType>(lhs: T?, rhs: String?) -> SDAI.STRING? { abstruct() }
-public func + <U: SDAIStringType>(lhs: String?, rhs: U?) -> SDAI.STRING? { abstruct() }
+//MARK: - String concatenation operator (12.5.2)
+
+public func + <T: SDAI__STRING__type, U: SDAI__STRING__type>(lhs: T?, rhs: U?) -> SDAI.STRING? { 
+	guard let lhs = lhs, let rhs = rhs else { return nil }
+	return SDAI.STRING( lhs.asSwiftType + rhs.asSwiftType )
+}
+public func + <T: SDAI__STRING__type, U: SDAISelectType>(lhs: T?, rhs: U?) -> SDAI.STRING? { lhs + rhs?.stringValue }
+public func + <T: SDAISelectType, U: SDAI__STRING__type>(lhs: T?, rhs: U?) -> SDAI.STRING? { lhs?.stringValue + rhs }
+public func + <T: SDAI__STRING__type>(lhs: T?, rhs: String?) -> SDAI.STRING? { lhs + SDAI.STRING(rhs) }
+public func + <U: SDAI__STRING__type>(lhs: String?, rhs: U?) -> SDAI.STRING? { SDAI.STRING(lhs) + rhs }
